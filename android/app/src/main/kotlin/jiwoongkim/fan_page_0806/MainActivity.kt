9c4dc62a6dc668ed9b07c69ecbf5be93a2dd50f0
Jiwoong Kim <jiwoongv@naver.om>
2020-08-14 10:36:48 +0900

    Initial commit

---
+++ android/app/src/main/kotlin/jiwoongkim/fan_page_0806/MainActivity.kt
@@ -0,0 +1,6 @@
+package jiwoongkim.fan_page_0806
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
+import 'dart:math';
+
+void main() {
+  return runApp(MaterialApp(
+      home: Scaffold(
+    backgroundColor: Colors.black,
+    body: FanPage(),
+  )));
+}
+
+class FanPage extends StatefulWidget {
+  @override
+  _FanPageState createState() => _FanPageState();
+}
+
+class _FanPageState extends State<FanPage> {
+  int number = 1;
+
+  void picture() {
+    setState(() {
+      number = Random().nextInt(4) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Column(children: <Widget>[
+        Row(
+          children: <Widget>[
+            Expanded(
+              child: FlatButton(
+                child:
+                    Image.asset('images/logo.jpg', height: 50.0, width: 100.0),
+              ),
+            ),
+            SizedBox(height: 100.0),
+          ],
+        ),
+        Row(children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                picture();
+              },
+              child: Image.asset('images/0$number.jpg'),
+            ),
+          )
+        ]),
+        Row(children: <Widget>[
+          SizedBox(height: 50.0),
+          FlatButton(
+              child: Text(
+            'Listen to IU Album',
+            style: TextStyle(
+                fontSize: 20.0,
+                color: Colors.white,
+                letterSpacing: 2.5,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'DancingScript'),
+          ))
+        ]),
+        Row(children: <Widget>[
+          Expanded(
+              child: FlatButton(
+                  onPressed: () {
+                    final player = AudioCache();
+                    player.play('Jazz01.mp3');
+                  },
+                  child: Image.asset('images/IU2.jpeg'))),
+          Expanded(
+              child: FlatButton(
+                  onPressed: () {
+                    final player = AudioCache();
+                    player.play('Jazz02.mp3');
+                  },
+                  child: Image.asset('images/IU4.jpeg'))),
+          Expanded(
+              child: FlatButton(
+                  onPressed: () {
+                    final player = AudioCache();
+                    player.play('Jazz03.mp3');
+                  },
+                  child: Image.asset('images/IU5.jpeg'))),
+        ]),
+        Row(children: <Widget>[
+          SizedBox(height: 50.0),
+          FlatButton(
+              child: Text(
+            'Music Player',
+            style: TextStyle(
+                fontSize: 20.0,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 2.5,
+                fontFamily: 'DancingScript'),
+          ))
+        ]),
+        Row(children: <Widget>[
+          FlatButton(
+              child: Image.asset('images/player0.jpeg',
+                  height: 180.0, width: 180.0)),
+          Container(
+              child: Image.asset('images/playlist1.jpg',
+                  height: 180.0, width: 200.0))
+        ]),
+        Row(children: <Widget>[
+          SizedBox(height: 100.0),
+          FlatButton(
+              child: Text(
+            'This particular fan app is made solely for the purpose of '
+            'personal study.',
+            style: TextStyle(
+                fontSize: 10.0,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 2.5,
+                fontFamily: 'DancingScript'),
+          ))
+        ]),
+      ]),
+    );
+  }
+}
